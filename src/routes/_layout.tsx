89e1c58e190918597f5e25da65667a094afb6f46
casualmeow <gleb.snurnikov@gmail.com>
2025-06-22 09:29:19 +0100

    layout padding fix

---
+++ src/routes/_layout.tsx
@@ -0,0 +1,23 @@
+import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { Sidebar } from '@/widgets/sidebar/ui/sidebar'
+import { Header } from '@/widgets/header/ui/header'
+
+export const Route = createFileRoute('/_layout')({
+  component: AppLayout,
+})
+
+function AppLayout() {
+  return (
+    <div className="bg-[#fafafa] min-h-screen">
+      <div className="flex p-4">
+        <Sidebar className="p-6 mr-2" />
+        <div className="bg-white rounded-xl w-full">
+          <Header />
+          <main className="p-6">
+            <Outlet />
+          </main>
+        </div>
+      </div>
+    </div>
+  )
+}
