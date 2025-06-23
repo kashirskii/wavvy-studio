a0020ab1edb89c5c9d2d3b1f7350bdfaf403f1cc
Gleb <70693755+casualmeow@users.noreply.github.com>
2025-06-23 03:36:55 +0300

    merge feature/sidebar 🦄
    * layout fix & sidebar decompose🦄
    
    * layout padding fix
    
    * merge sidebar
    
    * refactor footer. added dialog and restructured logic inside of it 🦄
    
    * ps script added inside gitignore. script merges from main branch into branch below 🦄
    
    * layout fix & sidebar decompose🦄
    
    * layout padding fix
    
    * merge sidebar
    
    * refactor footer. added dialog and restructured logic inside of it 🦄
    
    * ps script added inside gitignore. script merges from main branch into branch below 🦄
    
    * fixed duplucating inside deps in package.json 🦄
    
    * 🦄

---
+++ src/widgets/sidebar/ui/footer.tsx
@@ -0,0 +1,42 @@
+import { SidebarSettingsDialog } from './settings'
+import { Button, 
+Sheet,
+SheetTrigger, 
+SheetContent, 
+Dialog,
+DialogTrigger,
+DialogTitle,
+DialogContent } from '@/shared/ui';
+import { cn } from '@/shared/lib/utils';
+
+export function SidebarFooter({
+  ...props
+}) {
+  return (
+    <div className={cn('space-y-2 p-2')} {...props}>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="ghost" className="w-full flex justify-start">
+            Settings
+            </Button>
+          </DialogTrigger>
+          <DialogContent className='min-w-[60vw] min-h-[80vh]'>
+            <DialogTitle>
+              Settings
+            </DialogTitle>
+            <SidebarSettingsDialog />
+          </DialogContent>
+        </Dialog>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="ghost" className="w-full flex justify-start">
+            Feedback
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="right">
+
+          </SheetContent>
+        </Sheet>
+    </div>
+  );
+}
