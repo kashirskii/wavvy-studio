ee15e78c89b8e2ec6ce1cc3a09ff4f4f3a541d47
kashirskii <kashirskiy.yegor@ya.ru>
2025-06-23 05:25:15 +0400

    styles change 🦄

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -12,9 +12,9 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
-      <div className="bg-muted min-h-screen">
-        <div className="p-4 flex">
-          <Sidebar className="p-6 mr-2" />
+      <div className="bg-sidebar min-h-screen">
+        <div className="p-4 flex items-start">
+          <Sidebar className="py-6 pr-6 mr-2" />
           <div className="bg-background rounded-xl shadow-sm border shrink-1 min-w-0 grow-1">
             <Header />
             <main className="p-6">
