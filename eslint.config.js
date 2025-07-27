25dc8e7ee153a41d897cda855d054e37066187b7
casualmeow <gleb.snurnikov@gmail.com>
2025-07-27 20:13:46 +0100

    feat: add eslint rules for external IDE's to provide correct linting 🦄

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,21 @@
 //  @ts-check
 
+import { fileURLToPath } from 'node:url'
+import { dirname } from 'node:path'
 import { tanstackConfig } from '@tanstack/eslint-config'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
   ...tanstackConfig,
   {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
     rules: {
       'sort-imports': 'off',
       'import/consistent-type-specifier-style': 'off',
